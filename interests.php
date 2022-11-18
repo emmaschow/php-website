4e51987b4cc4aaf67979e827b81e497e04bb42fc
emmaschow <117453402+emmaschow@users.noreply.github.com>
2022-11-18 07:49:26 -0500

    Update interests.php

--- interests.php
+++ interests.php
@@ -8,21 +8,21 @@ include 'includes/header.php'; // Don't change this.
 
 <h1>Interests</h1>
 
-     <h2>Ten Cultural Works that have Most Influenced my Life</h2>
+     <h2>Ten Books that have Most Influenced my Life</h2>
        <ol>
          <li><a href="https://www.amazon.com/Grendel-John-Gardner/dp/0679723110" target="_blank">Grendel by John Gardner</a> (example absolute link that will open in a new window or tab)</li>
-         <li>We Wish to Inform You That Tomorrow We Will Be Killed with Our Families by Philip Gourevitch</li>
-         <li>Heads, You Lose, and Other Apocryphal Tales by Francis Greig</li>
-         <li>The Great Gatsby by F. Scott Fitzgerald</li>
-         <li>Angels and Demons by Dan Brown</li>
-         <li>Fahrenheit 451 by Ray Bradbury</li>
-         <li>Of Mice and Men by John Steinbeck</li>
-         <li>The Outsiders by S.E. Hinton</li>
-         <li>1984 by George Orwell</li>
-         <li>Jonathan Livingston Seagull by Richard Bach</li>
+         <li>When Breath Becomes Air by Paul Kalanithi</li>
+         <li>Between Shades of Gray by Ruta Sepetys</li>
+         <li>The Book Thief by Markus Zusak</li>
+         <li>To Kill a Mockingbird by Harper Lee</li>
+         <li>The Odyssey by Homer</li>
+         <li>Frankenstein by Mary Shelley</li>
+         <li>The Call of the Wild by Jack London</li>
+         <li>Macbeth by William Shakespeare</li>
+         <li>Antigone by Sophocles</li>
        </ol>
 
-       <p>Although I love education and improving my mind in class, I also manage to stay occupied during my free time. I am in the marching band at UNC, which takes up most of my evenings during the week. I also enjoy yoga and try to do it as much as possible, but it has been difficult to maintain a schedule for that while at school this year. Spending time with my dog is my favorite way to pass time. She is a border collie, and she loves to catch frisbees.</p>
+       <p>Although I love learning, I also have several other hobbies that I enjoy doing. I like to go on hikes and try to do it at least three times a week, but it can be challenging to schedule around my academic responsabilites. Another thing I enjoy doing with my time is spending time with my friends and family. Being around my community allows me to recharge and take a break.</p>
 
        	     <img class="img-fluid" src="images/border-collie-frisbee.jpg" alt="Border Collie Catching Frisbee">
 
