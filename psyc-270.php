ecf0f5b24609adb39165d878eb282e8e125b6c50
emmaschow <117453402+emmaschow@users.noreply.github.com>
2022-11-18 07:59:17 -0500

    Update and rename class-3.php to psyc-270.php

--- psyc-270.php
+++ psyc-270.php
@@ -1,14 +1,15 @@
 <?php
-$pageTitle = 'Class 3';
+$pageTitle = 'PSYC 270';
 $bkgdImage = 'rocks.jpg';
 include 'includes/header.php'; // Don't change this.
 ?>
 
 
     <?php include 'includes/class-nav.php'; ?>
-<h1>Class 3</h1>
+<h1>PSYC 270</h1>
 
-<p>This is my class 3.</p>
+<a href="https://sakai.unc.edu/portal/site/572a8196-1653-4e68-9b66-413e0f14c4d3">PSYC 270</a>
+<p>This class examines research methods in psychology.</p>
 
 <?php include 'includes/footer.php'; // Don't Change this
 ?>
