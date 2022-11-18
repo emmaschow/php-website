aa3943e386c49137aba9f8f53e4d2bba0557a9ec
emmaschow <117453402+emmaschow@users.noreply.github.com>
2022-11-18 07:52:19 -0500

    Update inls-161.php

--- inls-161.php
+++ inls-161.php
@@ -9,7 +9,8 @@ include 'includes/header.php'; // Don't change this.
     <?php include 'includes/class-nav.php'; ?>
 <h1>INLS 161</h1>
 
-<p>INLS is .....</p>
+<a href="https://ils.unc.edu/courses/2022_fall/inls161_001/">INLS 161</a>
+<p>INLS 161 explores the Tools and concepts necessary for information literacy</p>
 
 
 
