f0bd9609ba7be3a93efb617d011faa2554a45354
emmaschow <117453402+emmaschow@users.noreply.github.com>
2022-11-22 15:42:57 -0800

    Update interests.php

--- interests.php
+++ interests.php
@@ -10,16 +10,16 @@ include 'includes/header.php'; // Don't change this.
 
      <h2>Ten Books that have Most Influenced my Life</h2>
        <ol>
-         <li><a href="https://www.amazon.com/Grendel-John-Gardner/dp/0679723110" target="_blank">Grendel by John Gardner</a> (example absolute link that will open in a new window or tab)</li>
-         <li>When Breath Becomes Air by Paul Kalanithi</li>
-         <li>Between Shades of Gray by Ruta Sepetys</li>
-         <li>The Book Thief by Markus Zusak</li>
-         <li>To Kill a Mockingbird by Harper Lee</li>
-         <li>The Odyssey by Homer</li>
-         <li>Frankenstein by Mary Shelley</li>
-         <li>The Call of the Wild by Jack London</li>
-         <li>Macbeth by William Shakespeare</li>
-         <li>Antigone by Sophocles</li>
+         <li><a href="https://www.amazon.com/Where-Fern-Grows-Wilson-Rawls/dp/0440412676/ref=sr_1_1?keywords=where+the+red+fern+grows&qid=1669160203&s=books&sprefix=where+the+re%2Cstripbooks%2C134&sr=1-1" target="_blank">Where the Red Fern Grows by Wilson Rawls</a></li>
+         <li><a href="https://www.amazon.com/When-Breath-Becomes-Air-audiobook/dp/B0191YTT70/ref=sr_1_1?keywords=when+breath+becomes+air+by+paul+kalanithi&qid=1669160248&s=books&sprefix=when+breath+b%2Cstripbooks%2C136&sr=1-1" target="_blank">When Breath Becomes Air by Paul Kalanithi</a></li>
+         <li><a href="https://www.amazon.com/Between-Shades-of-Gray-audiobook/dp/B004T7CXX2/ref=sr_1_1?keywords=between+shades+of+gray+ruta+sepetys&qid=1669160307&s=audible&sprefix=between+shad%2Caudible%2C132&sr=1-1" target="_blank">Between Shades of Gray by Ruta Sepetys</a></li>
+         <li><a href="https://www.amazon.com/The-Book-Thief-Markus-Zusak-audiobook/dp/B000J20TZA/ref=sr_1_1?crid=NSUJLMHX9ULM&keywords=the+book+thief&qid=1669160414&s=audible&sprefix=the+book+thief%2Caudible%2C134&sr=1-1" target="_blank">The Book Thief by Markus Zusak</a></li>
+         <li>To Kill a Mockingbird by Harper Lee</a></li>
+         <li>The Odyssey by Homer</a></li>
+         <li>Frankenstein by Mary Shelley</a></li>
+         <li>The Call of the Wild by Jack London</a></li>
+         <li>Macbeth by William Shakespeare</a></li>
+         <li>Antigone by Sophocles</a></li>
        </ol>
 
        <p>Although I love learning, I also have several other hobbies that I enjoy doing. I like to go on hikes and try to do it at least three times a week, but it can be challenging to schedule around my academic responsabilites. Another thing I enjoy doing with my time is spending time with my friends and family. Being around my community allows me to recharge and take a break.</p>
