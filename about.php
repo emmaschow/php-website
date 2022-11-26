1fe716e85125cf996f1259be9514b240c4219371
emmaschow <117453402+emmaschow@users.noreply.github.com>
2022-11-26 17:03:07 -0500

    Update about.php

--- about.php
+++ about.php
@@ -10,6 +10,8 @@ include 'includes/header.php';
 
 <p>Hi there, I am Emma Chow, a second year student at UNC-Chapel Hill in North Carolina. I am 19-years-old and orginally from Greensboro, NC. I am a dedicated student studying Information science and Psychology. Ever since I was a kid, I've wanted to explore how information systems function and how they can be ultized to help people. As an undergraduate, I am continuing to explore my chosen majors and hope to expand my knowledage as I progess at UNC. When I am not in the classroom, I love to hike, play with my three dogs, and see my friends and family. </p>
 
+      <img class="img-fluid" src="images/psyc.jpg" alt="Psychology books">
+
 
 
 <?php  // include footer
