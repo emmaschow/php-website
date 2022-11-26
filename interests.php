fad90f43ab118523a1f234382f8e57162ca80a74
emmaschow <117453402+emmaschow@users.noreply.github.com>
2022-11-26 16:39:31 -0500

    Update interests.php

--- interests.php
+++ interests.php
@@ -14,15 +14,15 @@ include 'includes/header.php'; // Don't change this.
          <li><a href="https://www.amazon.com/When-Breath-Becomes-Air-audiobook/dp/B0191YTT70/ref=sr_1_1?keywords=when+breath+becomes+air+by+paul+kalanithi&qid=1669160248&s=books&sprefix=when+breath+b%2Cstripbooks%2C136&sr=1-1" target="_blank">When Breath Becomes Air by Paul Kalanithi</a></li>
          <li><a href="https://www.amazon.com/Between-Shades-of-Gray-audiobook/dp/B004T7CXX2/ref=sr_1_1?keywords=between+shades+of+gray+ruta+sepetys&qid=1669160307&s=audible&sprefix=between+shad%2Caudible%2C132&sr=1-1" target="_blank">Between Shades of Gray by Ruta Sepetys</a></li>
          <li><a href="https://www.amazon.com/The-Book-Thief-Markus-Zusak-audiobook/dp/B000J20TZA/ref=sr_1_1?crid=NSUJLMHX9ULM&keywords=the+book+thief&qid=1669160414&s=audible&sprefix=the+book+thief%2Caudible%2C134&sr=1-1" target="_blank">The Book Thief by Markus Zusak</a></li>
-         <li>To Kill a Mockingbird by Harper Lee</a></li>
-         <li>The Odyssey by Homer</a></li>
-         <li>Frankenstein by Mary Shelley</a></li>
-         <li>The Call of the Wild by Jack London</a></li>
-         <li>Macbeth by William Shakespeare</a></li>
-         <li>Antigone by Sophocles</a></li>
+         <li><a href = "https://www.amazon.com/Kill-Mockingbird-Harper-Lee/dp/0446310786" target="_blank"> To Kill a Mockingbird by Harper Lee</a></li>
+         <li><a href = "https://www.amazon.com/Odyssey-Homer/dp/0140268863/ref=sr_1_1?keywords=the+odyssey+by+homer&qid=1669498484&s=books&sprefix=the+odyessey+by+home%2Cstripbooks%2C97&sr=1-1"  target="_blank"> The Odyssey by Homer</a></li>
+         <li><a href = " https://www.amazon.com/Frankenstein-Mary-Shelley/dp/1673287883/ref=sr_1_1_sspa?keywords=frankenstein+mary+shelley&qid=1669498531&s=books&sprefix=franken%2Cstripbooks%2C85&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzMTczNlVaRDVRS0dIJmVuY3J5cHRlZElkPUEwODE1NzQ1MVpWRjQwSUQ2VEEzMSZlbmNyeXB0ZWRBZElkPUEwMzQ2OTQ4N1UzS05HV1JPNElTJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=1-1"  target="_blank"> Frankenstein by Mary Shelley</a></li>
+         <li><a href = "https://www.amazon.com/Call-Wild-Jack-London/dp/167334951X/ref=sr_1_1?keywords=the+call+of+the+wild+by+jack+london&qid=1669498591&s=books&sprefix=the+call+%2Cstripbooks%2C79&sr=1-1" target="_blank"> The Call of the Wild by Jack London</a></li>
+         <li><a href = "https://www.amazon.com/Macbeth-Folger-Shakespeare-Library-William/dp/0743477103/ref=sr_1_1?keywords=macbeth+by+william+shakespeare&qid=1669498629&s=books&sprefix=macbeth+%2Cstripbooks%2C72&sr=1-1" target="_blank"> Macbeth by William Shakespeare</a></li>
+         <li><a href = "https://www.amazon.com/Antigone-Sophocles/dp/1580493882/ref=sr_1_1?keywords=antigone+sophocles&qid=1669498655&s=books&sprefix=antigone+%2Cstripbooks%2C78&sr=1-1" target="_blank"> Antigone by Sophocles</a></li>
        </ol>
 
-       <p>Although I love learning, I also have several other hobbies that I enjoy doing. I like to go on hikes and try to do it at least three times a week, but it can be challenging to schedule around my academic responsabilites. Another thing I enjoy doing with my time is spending time with my friends and family. Being around my community allows me to recharge and take a break.</p>
+       <p>When I am not in the classroom, I have several other hobbies that I enjoy doing. I like to go on hikes and try to do it at least three times a week, but it can be challenging to schedule around my academic responsabilites. Another thing I enjoy doing with my time is spending time with my friends and family. Being around my community allows me to recharge and take a break.</p>
 
        	     <img class="img-fluid" src="images/border-collie-frisbee.jpg" alt="Border Collie Catching Frisbee">
 
