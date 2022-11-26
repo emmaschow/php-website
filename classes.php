ebba442e26bd2c11186ee6ea23405d46447f4770
emmaschow <117453402+emmaschow@users.noreply.github.com>
2022-11-26 17:05:14 -0500

    Update classes.php

--- classes.php
+++ classes.php
@@ -11,5 +11,8 @@ include 'includes/header.php'; // Don't change this.
 
 <p>Click on each link to learn more about my classes!</p>
 
+      <img class="img-fluid" src="images/class.jpg" alt="Classroom">
+
+
 <?php include 'includes/footer.php'; // Don't Change this
 ?>
