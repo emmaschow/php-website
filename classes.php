a6b4b7423ce1d6b50da8f5a981880916d179d079
emmaschow <117453402+emmaschow@users.noreply.github.com>
2022-11-28 19:11:26 -0500

    Update classes.php

--- classes.php
+++ classes.php
@@ -6,7 +6,7 @@ include 'includes/header.php'; // Don't change this.
 
 <!-- THIS IS THE UNIQUE CONTENT FOR EACH PAGE! -->
 
-<?php include 'includes/class-nav.php'; ?>
+<?php include 'includes/class-nav-bar2.php'; ?>
 <h1>Fall 2022 Courses</h1>
 
 <p>Click on each link to learn more about my classes!</p>
