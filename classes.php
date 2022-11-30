a4525dbca965cde09a9d508bd2e670dbec735939
emmaschow <117453402+emmaschow@users.noreply.github.com>
2022-11-30 02:17:23 -0500

    Update classes.php

--- classes.php
+++ classes.php
@@ -6,7 +6,7 @@ include 'includes/header.php'; // Don't change this.
 
 <!-- THIS IS THE UNIQUE CONTENT FOR EACH PAGE! -->
 
-<?php include 'includes/class-nav-bar2.php'; ?>
+<?php include 'includes/class-nav.php'; ?>
 <h1>Fall 2022 Courses</h1>
 
 <p>Click on each link to learn more about my classes!</p>
@@ -14,5 +14,4 @@ include 'includes/header.php'; // Don't change this.
       <img class="img-fluid" src="images/class.jpg" alt="Classroom">
 
 
-<?php include 'includes/footer.php'; // Don't Change this
-?>
+<?php include 'includes/footer.php'; // Don't Change this?>
